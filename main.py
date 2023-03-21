924a7292ecddf5fc6c39b6f2e7d11ac294631110
Yoon <74040890+memoday@users.noreply.github.com>
2023-03-21 16:51:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from datetime import datetime
-import socket
 
 app = FastAPI()
 # app.mount("/", StaticFiles(directory="public", html = True), name="static")
@@ -9,7 +8,6 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    testNum = datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d %H:%M:%S %Z%z')
-    myIp = socket.gethostbyname(socket.gethostname())
-    return {"date": testNum,"IP": myIp}
+    testNum = datetime.now()
+    return {"date": testNum}
 
