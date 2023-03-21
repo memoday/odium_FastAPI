3bbb1ff1638c9f6b453f4da055c318347b8e4382
Yoon <74040890+memoday@users.noreply.github.com>
2023-03-21 16:44:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    testNum = datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d %H:%M:%S %Z%z')
+    testNum = datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d %H:%M:%S %Z%z')
     myIp = socket.gethostbyname(socket.gethostname())
     return {"date": testNum,"IP": myIp}
 
