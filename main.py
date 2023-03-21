5c357b860638141cecc15aecec45856415ce9f57
Yoon <74040890+memoday@users.noreply.github.com>
2023-03-21 16:02:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from datetime import datetime
 
 app = FastAPI()
-app.mount("/", StaticFiles(directory="public", html = True), name="static")
+# app.mount("/", StaticFiles(directory="public", html = True), name="static")
+
+
+@app.get("/")
+async def root():
+    testNum = datetime.now()
+    return {"date": testNum}
+
