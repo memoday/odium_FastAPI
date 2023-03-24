00b127eb1c9721ba5d56165f1711f30761fa0158
Yoon <74040890+memoday@users.noreply.github.com>
2023-03-24 16:10:12 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from datetime import datetime
+from datetime import datetime, timedelta
 from pytz import timezone
 
 app = FastAPI()
 # app.mount("/", StaticFiles(directory="public", html = True), name="static")
 
 def symbolCount():
-    today = datetime.now(timezone('Asia/Seoul')).strftime('%Y-%m-%d')
-    today = datetime.strptime(today,'%Y-%m-%d')
+    today = datetime.now(timezone('Asia/Seoul')).strftime('%Y.%m.%d')
+    td = today
+    today = datetime.strptime(today,'%Y.%m.%d')
 
     nowValue = 742
     i = 0
     maxLevel = [0, 29, 76, 141, 224, 325, 444, 581, 736, 909, 1100]
     nowLevel = i
-    publishedDate = datetime.strptime('2022-12-09','%Y-%m-%d')
+    publishedDate = datetime.strptime('2022.12.09','%Y.%m.%d')
 
     difference = today - publishedDate
     
@@ -28,11 +29,37 @@ def symbolCount():
         i += 1
         nowLevel = i
 
-    return nowLevel, nowValue
+    nowLevelMaxValue = maxLevel[i]
+    countsToNextLevel = maxLevel[i] - nowValue
+    daysToNextLevel = int(countsToNextLevel/5) #math.ceil 써야지 올림됨 임시!!
+    dateToNextLevel = today + timedelta(days=daysToNextLevel)
+    dateToNextLevel = datetime.strftime(dateToNextLevel,'%Y.%m.%d')
+
+    global symbol
+    symbol = {
+        'date' : td,
+        'currentLevel' : nowLevel,
+        'currentValue' : nowValue,
+        'currentLevelMaxValue' : nowLevelMaxValue,
+        'countsToNextLevel' : countsToNextLevel,
+        'daysToNextLevel' : daysToNextLevel,
+        'dateToNextLevel' : dateToNextLevel,
+    }
+
+    return symbol
 
 @app.get("/")
 async def root():
     nowLevel, nowValue = symbolCount()
-    testNum = datetime.now(timezone('Asia/Seoul')).strftime('%Y-%m-%d')
-    return {"nowLevel" : nowLevel, "nowValue" : nowValue,"date": testNum}
+    return {"date": symbol['date'],
+            "currentLevel" : symbol['currentLevel'], 
+            "currentValue" : symbol['currentValue'],
+            "currentLevelMaxValue" : symbol['currentLevelMaxValue'],
+            "countsToNextLevel" : symbol['countsToNextLevel'],
+            "daysToNextLevel" : symbol['daysToNextLevel'],
+            "dateToNextLevel" : symbol['dateToNextLevel'],
+            }
+
+if __name__ == "__main__":
+    print(symbolCount())
 
