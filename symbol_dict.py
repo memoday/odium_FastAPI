e5b5214bd02efb596b8dd67a96543246d9edb55e
memoday <74040890+memoday@users.noreply.github.com>
2023-06-15 23:16:34 +0900

    Update symbol-odium
    오디움 일퀘 개수 증가에 따른 값 수정

--- symbol_dict.py
+++ symbol_dict.py
@@ -6,13 +6,13 @@ import pytz
 
 symbols = { #기본 설정
     "odium" : { #오디움
-        "startValue" : 742, #첫 심볼 개수
-        "additionValue" : 380, #이벤트 심볼 개수
-        "dailyValue" : 5, #일퀘 개수
-        "releaseDate" : datetime.strptime('2022.12.09', '%Y.%m.%d'),
+        "startValue" : 2067, #첫 심볼 개수
+        "additionValue" : 0, #이벤트 심볼 개수
+        "dailyValue" : 10, #일퀘 개수
+        "releaseDate" : datetime.strptime('2023.06.15', '%Y.%m.%d'),
     },
     "shangrila" : { #도원경
-        "startValue" : 10,
+        "startValue" : 11,
         "additionValue" : 0,
         "dailyValue" : 10,
         "releaseDate" : datetime.strptime('2023.06.15', '%Y.%m.%d'),
